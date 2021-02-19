52b70ee377a8ca5c5f8098293e3169cf1dfb0700
Felipe Jung <felipemattoseu@gmail.com>
2021-02-18 21:32:49 -0400

    feat(signupcontroller): ensure a 400 status code return if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { badRequest } from '../helpers/http-helper';
 
 export default class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email'];
+    const requiredFields = ['name', 'email', 'password'];
 
     const bodyKeys = Object.keys(httpRequest.body);
 
