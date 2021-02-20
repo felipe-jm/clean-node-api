8b70b67d62d0c41e67fb7cf95ddbc8ca50b1d2e2
Felipe Jung <felipemattoseu@gmail.com>
2021-02-20 10:43:16 -0400

    refactor(signup.ts): move all error imports to a single file

---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,5 @@
+import MissingParamError from './missing-param-error';
+import InvalidParamError from './invalid-param-error';
+import ServerError from './server-error';
+
+export { MissingParamError, InvalidParamError, ServerError };
