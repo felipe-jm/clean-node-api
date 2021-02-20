c67aede5c6f9a879c0658178a52ae9d30d7f7cb1
Felipe Jung <felipemattoseu@gmail.com>
2021-02-20 12:30:30 -0400

    feat(email-validator): ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++
@@ -1,9 +0,0 @@
-import { EmailValidatorAdapter } from './email-validator';
-
-describe('EmailValidator Adapter', () => {
-  it('should return false if validator returns false', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter();
-    const isValid = emailValidatorAdapter.isValid('invalid_email@email.com');
-    expect(isValid).toBe(false);
-  });
-});
