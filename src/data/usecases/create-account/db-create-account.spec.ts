74dc634712b6a1f2b0e2017bae5f8d54d27d80a1
Felipe Jung <felipemattoseu@gmail.com>
2021-02-21 10:33:54 -0400

    refactor(dbcreateaccount): move DbCreateAccount creation to a factory helper method

--- src/data/usecases/create-account/db-create-account.spec.ts
+++ src/data/usecases/create-account/db-create-account.spec.ts
@@ -1,14 +1,28 @@
+import { Encrypter } from '../../protocols/encrypter';
 import DbCreateAccount from './db-create-account';
+
+interface MakeDbCreateAccountReturn {
+  dbCreateAccount: DbCreateAccount;
+  encrypterMock: Encrypter;
+}
+
+const makeDbCreateAccount = (): MakeDbCreateAccountReturn => {
+  class EncrypterMock {
+    async encrypt(password: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'));
+    }
+  }
+  const encrypterMock = new EncrypterMock();
+  const dbCreateAccount = new DbCreateAccount(encrypterMock);
+  return {
+    dbCreateAccount,
+    encrypterMock,
+  };
+};
 
 describe('DbCreateAccount Usecase', () => {
   it('should call Encrypter with correct password', async () => {
-    class EncrypterMock {
-      async encrypt(password: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'));
-      }
-    }
-    const encrypterMock = new EncrypterMock();
-    const dbCreateAccount = new DbCreateAccount(encrypterMock);
+    const { dbCreateAccount, encrypterMock } = makeDbCreateAccount();
     const encryptSpy = jest.spyOn(encrypterMock, 'encrypt');
     const accountData = {
       name: 'valid_name',
