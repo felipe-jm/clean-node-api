a330f092607fa5d6f86925c8d585423a9c29477c
Felipe Jung <felipemattoseu@gmail.com>
2021-02-21 10:59:22 -0400

    feat(dbcreateaccount): ensure DbCreateAccount calls CreateAccountRepository with correct values

--- src/data/usecases/create-account/db-create-account.ts
+++ src/data/usecases/create-account/db-create-account.ts
@@ -3,17 +3,28 @@ import {
   CreateAccountModel,
   AccountModel,
   Encrypter,
+  CreateAccountRepository,
 } from './db-create-account-procotols';
 
 export default class DbCreateAccount implements CreateAccount {
   private readonly encrypter: Encrypter;
 
-  constructor(encrypter: Encrypter) {
+  private readonly createAccountRepository: CreateAccountRepository;
+
+  constructor(
+    encrypter: Encrypter,
+    createAccountRepository: CreateAccountRepository,
+  ) {
     this.encrypter = encrypter;
+    this.createAccountRepository = createAccountRepository;
   }
 
-  async create(account: CreateAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password);
+  async create(accountData: CreateAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    await this.createAccountRepository.create({
+      ...accountData,
+      password: hashedPassword,
+    });
     return new Promise(resolve => resolve(null));
   }
 }
